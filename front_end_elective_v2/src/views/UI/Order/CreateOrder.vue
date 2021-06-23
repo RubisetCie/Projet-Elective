132ff21b58773e2da843bd09dc55d81c8188f811
julienliguori <julien.liguori@viacesi.fr>
2021-06-23 11:36:33 +0200

    mise en place de vue en V2

---
+++ front_end_elective_v2/src/views/UI/Order/CreateOrder.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import Options from 'vue-class-component';
+import Vue from 'vue';
+
+@Options({
+  components: {},
+  data: () => ({
+    
+  }),
+})
+export default class Account extends Vue {}
+</script>
