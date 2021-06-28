70c95b927ee4e7c25cbe7659fa3f4b2d18df07c7
Matthieu Carteron <rubisetcie@gmail.com>
2021-06-28 17:39:30 +0200

    Added foreign keys to include multiple addresses and billings.

--- database-sql/cleanup.sql
+++ database-sql/cleanup.sql
@@ -1,2 +1,21 @@
--- User table deletion
-DROP TABLE dbo.users;
+-- Removing all foreign key constraints
+WHILE (EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_TYPE='FOREIGN KEY'))
+BEGIN
+	DECLARE @sql nvarchar(2000)
+	SELECT TOP 1 @sql=('ALTER TABLE ' + TABLE_SCHEMA + '.[' + TABLE_NAME + '] DROP CONSTRAINT [' + CONSTRAINT_NAME + ']')
+	FROM information_schema.table_constraints
+	WHERE CONSTRAINT_TYPE = 'FOREIGN KEY'
+	EXEC (@sql)
+	PRINT @sql
+END
+
+-- Removing all tables
+WHILE (EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME != '__MigrationHistory' AND TABLE_TYPE = 'BASE TABLE'))
+BEGIN
+	DECLARE @sql nvarchar(2000)
+	SELECT TOP 1 @sql=('DROP TABLE ' + TABLE_SCHEMA + '.[' + TABLE_NAME + ']')
+	FROM INFORMATION_SCHEMA.TABLES
+	WHERE TABLE_NAME != '__MigrationHistory' AND TABLE_TYPE = 'BASE TABLE'
+	EXEC (@sql)
+	PRINT @sql
+END
--- database-sql/insert.sql
+++ database-sql/insert.sql
@@ -1,7 +1,35 @@
--- Add users into database
-INSERT INTO dbo.users("username", "usertype", "email", "password", "firstname", "lastname", "country", "zipcode", "city", "address", "billingnumber", "billincrypto", "billinowner")
+-- Add adresses into database
+INSERT INTO dbo.address("country", "zipcode", "city", "address")
 VALUES
-	('dupontmark', 0, 'mark.dupont@yopmail.com', 'bcrypt', 'Dupont', 'Mark', 'France', '75003', 'Paris', '42 rue du General de Gaule', '4242424242424242', '042', 'Mohamed Belgacem'),
-	('jeanmichel123', 1, 'jeanmichel@yopmail.com', 'azerty', 'Jean', 'Michel', 'France', '75003', 'Paris', '42 rue du General de Gaule', '4242424242424242', '042', 'Mohamed Belgacem'),
-	('francisboulanger', 3, 'francisboulanger@yopmail.com', 'incorrect', 'Francis', 'Boulanger', 'France', '75003', 'Paris', '42 rue du General de Gaule', '4242424242424242', '042', 'Mohamed Belgacem'),
-	('natasha', 5, 'n.anselm@yopmail.com', 'h1ghl@nd3r', 'Natasha', 'Anselm', 'France', '75003', 'Paris', '42 rue du General de Gaule', '4242424242424242', '042', 'Mohamed Belgacem')
+	('France', '75003', 'Paris', '42 rue du General de Gaule'),
+	('France', '92000', 'Nanterre', '93 Boulevard de la Seine')
+
+-- Add billings into database
+INSERT INTO dbo.billing("number", "crypto", "owner")
+VALUES
+	('4242424242424242', '042', 'Mohamed Belgacem')
+
+-- Add users into database
+INSERT INTO dbo.users("username", "usertype", "email", "password", "firstname", "lastname")
+VALUES
+	('dupontmark', 0, 'mark.dupont@yopmail.com', 'bcrypt', 'Dupont', 'Mark'),
+	('jeanmichel123', 1, 'jeanmichel@yopmail.com', 'azerty', 'Jean', 'Michel'),
+	('francisboulanger', 3, 'francisboulanger@yopmail.com', 'incorrect', 'Francis', 'Boulanger'),
+	('natasha', 5, 'n.anselm@yopmail.com', 'h1ghl@nd3r', 'Natasha', 'Anselm')
+
+-- Link the Users to the Addresses
+INSERT INTO dbo.useraddress("userid", "addressid")
+VALUES
+	(0, 0),
+	(1, 0),
+	(2, 1),
+	(3, 0),
+	(3, 1)
+
+-- Link the Users to the Billings
+INSERT INTO dbo.userbilling("userid", "billingid")
+VALUES
+	(0, 0),
+	(1, 0),
+	(2, 0),
+	(3, 0)
