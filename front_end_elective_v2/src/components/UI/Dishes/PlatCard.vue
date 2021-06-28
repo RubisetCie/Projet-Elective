99bb2f08c93f56e80efc12fb5f7d2db07efc0e46
hugoleboennec <hugo.leboennec@viacesi.fr>
2021-06-28 19:20:58 +0200

    add axios to menu page

--- front_end_elective_v2/src/components/UI/Dishes/PlatCard.vue
+++ front_end_elective_v2/src/components/UI/Dishes/PlatCard.vue
@@ -1,39 +1,34 @@
 <template>
   <div>
-   <v-card
-    class='mx-auto'
-    max-width='344'
-    outlined
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class='text-overline mb-4'>
-          OVERLINE
-        </div>
-        <v-list-item-title class='text-h5 mb-1'>
-          Headline 5
-        </v-list-item-title>
-        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-      </v-list-item-content>
+    <v-card class="mx-auto" max-width="844" outlined>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="text-overline mb-4">Menu</div>
+          <v-list-item-title class="text-h5 mb-1">
+            {{ info.name }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <ul>
+              <li v-for="plate in info.items" :key="plate">{{ plate }}</li>
+            </ul>
+          </v-list-item-subtitle>
+        </v-list-item-content>
 
-      <v-list-item-avatar
-        tile
-        size='80'
-        color='grey'
-      ></v-list-item-avatar>
-    </v-list-item>
+        <v-list-item-avatar
+          tile
+          size="80"
+        >        <v-img
+          :src="info.image.url"
+          height="80px"
+          with="80px"
+        ></v-img></v-list-item-avatar>
 
-    <v-card-actions>
-      <v-btn
-        outlined
-        rounded
-        text
-        @click='reserve'
-      >
-        Button
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-btn outlined rounded text @click="reserve()"> Ajouter </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
@@ -42,12 +37,14 @@ import Options from 'vue-class-component';
 import Vue from 'vue';
 
 @Options({
+  props: {
+    info: Object,
+  },
   methods: {
     reserve() {
-      console.log('test test test test');
-      this.$router.push('/cart');
-      // this.loading = true
-      // setTimeout(() => (this.loading = false), 2000)
+      return 0;
+
+      // TA MERE VUE X
     },
   },
 })
--- front_end_elective_v2/src/store/index.ts
+++ front_end_elective_v2/src/store/index.ts
@@ -5,8 +5,15 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // basket: [],
   },
   mutations: {
+    // resetBasket(state) {
+    //   state.basket = empt;
+    // },
+    // addToBasket(state, itemId) {
+    // state.basket.push({itemId});
+    // },
   },
   actions: {
   },
