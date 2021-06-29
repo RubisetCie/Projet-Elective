1b122c8041ff244531890fd4e2605b9ebff8f3b4
hugoleboennec <hugo.leboennec@viacesi.fr>
2021-06-29 09:27:14 +0200

    search bar

--- front_end_elective_v2/src/components/UI/HomeBar/HomeBar.vue
+++ front_end_elective_v2/src/components/UI/HomeBar/HomeBar.vue
@@ -8,8 +8,8 @@
         <v-spacer></v-spacer>
 
         <v-menu offset-y>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn icon v-bind="attrs" v-on="on">
+          <template v-slot:activator='{ on, attrs }'>
+            <v-btn icon v-bind='attrs' v-on='on'>
               <v-badge color='green' content='3'>
                 <v-icon>mdi-bell</v-icon>
               </v-badge>
@@ -31,13 +31,36 @@
         <v-btn icon @click='showSearchBar()'>
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
-        <v-text-field
-          v-show='searchBar'
-          single-line
-          solo
-          style="margin-top: 30px;"
-        >
-        </v-text-field>
+          <v-text-field
+            v-show='searchBar'
+            single-line
+            solo
+            @change='search()'
+            style='margin-top: 30px'
+          >
+          </v-text-field>
+          <div class='result__div' v-if='result && searchBar'>
+            <v-list three-line>
+              <router-link :to="'/menue' + rep.restaurantId"
+              v-for='rep in result' :key='rep.restaurantId'>
+              <!-- v-for='(rep, index) in result' :key='rep.restaurantId'> -->
+                <v-list-item :key='rep.name'>
+                  <!-- <v-divider v-if='index != 0'></v-divider><br/> -->
+                  <v-list-item-avatar>
+                    <v-img :src='rep.image.url'></v-img>
+                  </v-list-item-avatar>
+
+                  <v-list-item-content>
+                    <v-list-item-title v-html='rep.name'></v-list-item-title>
+                    <v-list-item-subtitle
+                      v-if='rep.description'
+                      v-html='rep.description'
+                    ></v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </router-link>
+            </v-list>
+        </div>
 
         <v-menu left bottom>
           <template v-slot:activator='{ on, attrs }'>
@@ -95,13 +118,15 @@
         <router-view />
       </v-main>
     </v-card>
-    <div v-if='result'></div>
   </div>
 </template>
 
 <script>
 import Options from 'vue-class-component';
 import Vue from 'vue';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'localhost:3000';
 
 @Options({
   components: {},
@@ -116,8 +141,37 @@ import Vue from 'vue';
     redirect(path) {
       this.$router.push(path);
     },
-    search() {
-      return 0;
+    async search(e) {
+      // const response = await axios.get(`/research?match=${this.restoId}`);
+      // this.result = response.data;
+      this.result = [
+        {
+          name: 'Istanbul Grill',
+          restaurantId: 'istanbul_4242',
+          address: '12 rue de la gare, Nanterre 92000',
+          status: 'open',
+          image: {
+            url: 'https://www.telerama.fr/sites/tr_master/files/styles/simplecrop1000/public/fauves13_0.jpg?itok=SAX57ak4',
+            name: 'dqz',
+          },
+          tags: ['kebab', 'grec', 'pas cher'],
+          opening: [{ open: '09:04:16+00:00', close: '22:04:16+00:00' }],
+          description: 'au kebab du soleil :)',
+        },
+        {
+          name: 'Grace Neo Pizza',
+          restaurantId: 'neo_4242',
+          address: '24 rue de la port, Marseille 92000',
+          status: 'open',
+          image: {
+            url: 'https://www.lesfilsamaman.com/wp-content/uploads/restaurant-a-orlean-1.jpg',
+            name: 'dqz',
+          },
+          opening: [{ open: '09:04:16+00:00', close: '22:04:16+00:00' }],
+          description: "au Pizza d'italie :)",
+
+        },
+      ];
     },
     showSearchBar() {
       this.searchBar = !this.searchBar;
@@ -126,3 +180,13 @@ import Vue from 'vue';
 })
 export default class HomeBar extends Vue {}
 </script>
+
+<style scoped>
+.result__div {
+  z-index: 50000;
+  position: relative;
+  top: 50%;
+  left: 0;
+  transform: translate(0, 50%);
+}
+</style>
