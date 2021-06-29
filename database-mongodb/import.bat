f3dc70b1f52a4876a308731283a6eae352be01f8
Matthieu Carteron <rubisetcie@gmail.com>
2021-06-29 11:53:58 +0200

    Added scripts to import data into the MongoDB database.

---
+++ database-mongodb/import.bat
@@ -0,0 +1,8 @@
+:: Import the menus
+mongoimport --uri "mongodb+srv://cluster-elective.n6ixa.mongodb.net/elective?authSource=%%24external&authMechanism=MONGODB-X509" --ssl --sslPEMKeyFile "..\..\X509-mongodb-admin.pem" --collection menus --file "menu.json"
+
+:: Import the restaurants
+mongoimport --uri "mongodb+srv://cluster-elective.n6ixa.mongodb.net/elective?authSource=%%24external&authMechanism=MONGODB-X509" --ssl --sslPEMKeyFile "..\..\X509-mongodb-admin.pem" --collection restaurants --file "restaurant.json"
+
+:: Import the orders
+mongoimport --uri "mongodb+srv://cluster-elective.n6ixa.mongodb.net/elective?authSource=%%24external&authMechanism=MONGODB-X509" --ssl --sslPEMKeyFile "..\..\X509-mongodb-admin.pem" --collection orders --file "order.json"
