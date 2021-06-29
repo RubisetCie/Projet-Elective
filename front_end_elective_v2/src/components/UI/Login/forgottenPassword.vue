2f5418086ce1d2f844d7ffa4486ee18c14ae2e0b
julienliguori <julien.liguori@viacesi.fr>
2021-06-29 16:27:57 +0200

    Login page

---
+++ front_end_elective_v2/src/components/UI/Login/forgottenPassword.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <v-list>
+        <div>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-lock</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-text-field label='Mot de passe'></v-text-field>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-lock</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-text-field label='Valider mot de passe'></v-text-field>
+            </v-list-item-content>
+          </v-list-item>
+        </div>
+      </v-list>
+  </div>
+</template>
+
+<script lang='ts'>
+import Options from 'vue-class-component';
+import Vue from 'vue';
+
+@Options({
+  methods: {},
+})
+export default class forgottenPassword extends Vue {}
+</script>
---
+++ front_end_elective_v2/src/views/Login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class='login'>
+    <v-card style='margin: 80px; margin-left: 200px; margint-right:200px;'>
+      <div v-if='know'>
+        <v-list>
+          <div>
+            <v-list-item>
+              <v-list-item-action>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-text-field label='Identifiant'></v-text-field>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-action>
+                <v-icon>mdi-lock</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-text-field label='Mot de passe'></v-text-field>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+        </v-list>
+        <v-row
+          align='center'
+          justify='space-around'
+          style='padding-bottom: 15px'
+        >
+          <div class='my-2'>
+            <v-btn x-small color='blue' @click='know = false'>
+              Mot de passe oublier
+            </v-btn>
+          </div>
+        </v-row>
+        <div class='d-flex justify-space-around' style='padding-bottom: 15px'>
+          <v-btn outlined rounded text @click='validate()'>
+            Valider
+          </v-btn>
+        </div>
+      </div>
+      <div v-else>
+        <forgottenPassword />
+        <div class='d-flex justify-space-around' style='padding-bottom: 15px'>
+          <v-btn outlined text @click='changePassword()'> Valider </v-btn>
+        </div>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import Options from 'vue-class-component';
+import Vue from 'vue';
+import forgottenPassword from '../components/UI/Login/forgottenPassword.vue';
+
+@Options({
+  data() {
+    return {
+      know: true,
+    };
+  },
+  components: {
+    forgottenPassword,
+  },
+  methods: {
+    validate() {
+      this.$router.push('/');
+    },
+    changePassword() {
+      this.know = true;
+    },
+  },
+})
+export default class Login extends Vue {}
+</script>
